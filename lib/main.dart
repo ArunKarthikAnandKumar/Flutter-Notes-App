21715face198c91c33621b1a8d8a5a45fbb496c4
ArunKarthikAnandKumar <137405963+ArunKarthikAnandKumar@users.noreply.github.com>
2024-05-27 14:25:46 +0530

    first commit
    first push

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () async{
+
+          },
+          child: const Text('Register'),
+        ),
+      ),
+    );
+  }
+}
